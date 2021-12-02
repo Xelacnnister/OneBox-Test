51bd7d35ebf684b291af48f0a7d2b03fe31404cd
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-12-02 11:54:43 +0100

    Creación Modules + Components

---
+++ src/app/ticket-app/pages/event-card/event-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styles: [
+  ]
+})
+export class EventCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ticket-app/pages/session-list/session-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-list',
+  templateUrl: './session-list.component.html',
+  styles: [
+  ]
+})
+export class SessionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
