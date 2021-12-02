d0281314ee30bd7d2512f8b40917a122316cccc0
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-12-02 14:03:32 +0100

    Creación de Rutas + depuración código

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './ticket-app/pages/home/home.component';
+import { SessionListComponent } from './ticket-app/pages/session-list/session-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'sesiones',
+    component: SessionListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './ticket-app/pages/home/home.component';
+import { SessionListComponent } from './ticket-app/pages/session-list/session-list.component';
+import { HeaderComponent } from './ticket-app/pages/header/header.component';
+import { EventCardComponent } from './ticket-app/pages/event-card/event-card.component';
+import { ShoppingCartComponent } from './ticket-app/pages/shopping-cart/shopping-cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SessionListComponent,
+    HeaderComponent,
+    EventCardComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
